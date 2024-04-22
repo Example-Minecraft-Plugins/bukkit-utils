863f27b89d6717358f816d221e4a62691e421baa
Davi Cunha <davi.pinheiro-costa-cunha@insa-lyon.fr>
2024-04-22 19:33:05 +0200

    Update InteractiveInventory.java
    Changed the NBTTags Map from a HashMap to a Map for better use when calling these methods

--- src/main/java/me/davipccunha/utils/inventory/InteractiveInventory.java
+++ src/main/java/me/davipccunha/utils/inventory/InteractiveInventory.java
@@ -8,9 +8,10 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InteractiveInventory {
-    public static ItemStack createToggleItem(boolean config, HashMap<String, String> nbtTags, String name, String description) {
+    public static ItemStack createToggleItem(boolean config, Map<String, String> nbtTags, String name, String description) {
         ItemStack wool = new ItemStack(Material.WOOL, 1, (short) (config ? 5 : 14));
 
         final List<String> lore = Arrays.asList(
@@ -21,7 +22,7 @@ public class InteractiveInventory {
         return createActionItem(wool, nbtTags, name, lore);
     }
 
-    public static ItemStack createActionItem(ItemStack item, HashMap<String, String> nbtTags, String name, List<String> lore) {
+    public static ItemStack createActionItem(ItemStack item, Map<String, String> nbtTags, String name, List<String> lore) {
         ItemStack actionItem = NBTHandler.addNBT(item, nbtTags);
 
         ItemMeta actionItemMeta = actionItem.getItemMeta();
